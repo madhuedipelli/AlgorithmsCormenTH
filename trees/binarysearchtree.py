69d59cdd947ac76f6f104e141e6f6621cafe88da
madhuedipelli <52343793+madhuedipelli@users.noreply.github.com>
2024-03-25 21:08:52 +0530

    add max,min, ceil, floor

--- trees/binarysearchtree.py
+++ trees/binarysearchtree.py
@@ -26,7 +26,7 @@ class BinarySearchTree:
         else:
             return self.search_helper(x.right,k)
     
-    def put(self,key,val):
+    def put(self,key,val=None):
         self.root = self.put_helper(self.root,key,val)
 
     def put_helper(self,x,key,val):
@@ -42,4 +42,66 @@ class BinarySearchTree:
             x.val = val
         
         return x
+    
+    def get_min(self):
+        if self.root is None :
+            return None
+        x = self.root
+        while x.left != None:
+            x = x.left
+        return x.key
+    
+    def get_max(self):
+        if self.root is None:
+            return None
+        x = self.root
+        while x.right is not None:
+            x = x.right
+        return x.key
+    
+    def floor(self, key):
+        x = self.floor_helper(self.root, key)
+        if x is None:
+            return None
+        return x.key
+    
+    def floor_helper(self, x, k):
+        if x is None:
+            return None
+        
+        if x.key == k:
+            return x
+        
+        if x.key > k :
+            return self.floor_helper(x.left, k)
+        # either x is floor or x right subtree has k's floor
+        # not left subtree
+        t = self.floor_helper(x.right, k)
+        if t is None:
+            return x
+        else:
+            return t
+    
+    def ceil(self,key):
+        x = self.ceil_helper(self.root, key)
+        if x :
+            return x.key
+        else:
+            return None
+    
+    def ceil_helper(self, x, k):
+        if x is None:
+            return None
+        if x.key == k:
+            return x
+        
+        if x.key < k:
+            return self.ceil_helper(x.right, k)
+        
+        # k's ceil is current x or 
+        # leftmost child in current x
 
+        t = self.ceil_helper(x.left, k)
+        if t is None:
+            return x
+        return t
--- trees/test_binarysearchtree.py
+++ trees/test_binarysearchtree.py
@@ -2,8 +2,16 @@ from binarysearchtree import BinarySearchTree
 
 test = BinarySearchTree()
 
-for i in range(10):
-    test.put(i,i*i)
+# for i in range(10):
+#     test.put(i,i*i)
+#     print(test.search(i))
+#     print(f'search key = {i} , search val = {test.search(i)} , max is {test.get_max()} and min is {test.get_min()}')
 
-for i in range(10):
-    print(test.search(i), end = ' ')
+test.put(12)
+test.put(7)
+test.put(3)
+test.put(5)
+test.put(4)
+
+print(test.floor(6))
+print(test.ceil(6))
